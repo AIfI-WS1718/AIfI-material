c858cd3a4c24416cfdc04562d6c417d5adbf51b3
u189548 <u189548@R-133.cadlab.tu-berlin.de>
2017-10-30 17:20:26 +0100

    changed amount of random numbers

--- src/vorlesung/staticMethods/Random.java
+++ src/vorlesung/staticMethods/Random.java
@@ -3,7 +3,7 @@ package vorlesung.staticMethods;
 public class Random {
 
 	public static void main(String[] args) {
-		for (int i=0; i<10; i++) {
+		for (int i=0; i<5; i++) {
 			System.out.println("random: " + Math.round(Math.random()*10));
 		}
 	}

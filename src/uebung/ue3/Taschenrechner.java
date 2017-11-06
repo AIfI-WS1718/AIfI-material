5eda5d795e24fc593df9c49eaef11d4533eca073
johannessturm <johannessturm@wlan-141-23-174-244.tubit.tu-berlin.de>
2017-11-06 17:36:32 +0100

    Taschenrechner fertig!

--- src/uebung/ue3/Taschenrechner.java
+++ src/uebung/ue3/Taschenrechner.java
@@ -1,7 +1,7 @@
 package uebung.ue3;
 
 public class Taschenrechner {
-
+	double ergebnis = 0;
 	public static void main(String[] args) {
 		Taschenrechner t = new Taschenrechner();
 		t.add(5);
@@ -11,12 +11,62 @@ public class Taschenrechner {
 		t.add(10);
 		t.substract(5);
 		System.out.println("Ergebnis: " + t.getResult());
+		t.reset();
+		t.add(3);
 		t.fakultaet();
 		System.out.println("Ergebnis: " + t.getResult());
+		t.reset();
+		t.add(9);
 		t.sqrt();
 		System.out.println("Ergebnis: " + t.getResult());
 	}
 
-	//Fehlende Methoden ergänzen:
+
+
+	private void sqrt() {
+		// TODO Auto-generated method stub
+		ergebnis=Math.sqrt(ergebnis);
+	}
+
+
+
+	private void fakultaet() {
+		// TODO Auto-generated method stub
+		if (ergebnis==0) {
+			ergebnis =1; 
+		}
+		else {
+			double temp;
+			temp = 1;
+			for (int j=1; j<=ergebnis; j++) {
+			temp = temp * j;
+			}
+			ergebnis = temp;
+		}
+	}
+
+
+
+	private void substract(int i) {
+		// TODO Auto-generated method stub
+		ergebnis = ergebnis - 5;
+	}
+
+	private double getResult() {
+		// TODO Auto-generated method stub
+		return ergebnis;
+	}
+
+	private void reset() {
+		// TODO Auto-generated method stub
+		ergebnis = 0;
+	}
+
+	private void add(int i) {
+		// TODO Auto-generated method stub
+		ergebnis = ergebnis + i;
+		
+	}
+
 
 }

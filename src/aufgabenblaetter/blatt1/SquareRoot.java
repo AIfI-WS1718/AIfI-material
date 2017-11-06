82fd7c562f5a8b32f899101b905619948a244f33
tthunig <thunig@vsp.tu-berlin.de>
2017-11-06 12:11:01 +0100

    datei fuer aufgabenblatt 1

---
+++ src/aufgabenblaetter/blatt1/SquareRoot.java
@@ -0,0 +1,10 @@
+package aufgabenblaetter.blatt1;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		// TODO bearbeite hier die Aufgabe aus dem freiwilligen Aufgabenblatt 1 und commite deine Loesung in deinem Branch. Viel Spaß :)
+		
+	}
+
+}

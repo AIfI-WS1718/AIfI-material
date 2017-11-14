fca0628072f4e967422ecf79dde3006ac5266d6f
tomhaubner <tomhaubner@toms-mbp-2>
2017-11-14 14:17:39 +0100

    neu

---
+++ src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int n = 1764;
+		int sqr = 0;
+		int result;
+		do{ sqr++;
+		      result = sqr * sqr;
+		    } while(result < n);
+		    if(result == n){
+		System.out.println("Die Wurzel aus " + n +" ist " + sqr); }else{
+		System.out.println("Keine Wurzel für " + n + " gefunden."); System.out.println("Die nächst größere Zahl mit einer Wurzel "+
+		"in den natürlichen Zahlen ist "+
+		result + " die Wurzel ist " + sqr); }
+		} 
+
+	}
+
+
---
+++ src/uebung4/Cow.java
@@ -0,0 +1,10 @@
+package uebung4;
+
+public class Cow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

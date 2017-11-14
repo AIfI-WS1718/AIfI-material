bb6a0ba8fab1fc477e859e1d16d80482eb449d8b
tthunig <thunig@vsp.tu-berlin.de>
2017-11-14 17:25:42 +0100

    Feedback zum Aufgabenblatt 1

--- src/aufgabenblaetter/blatt1/Console.java
+++ src/aufgabenblaetter/blatt1/Console.java
@@ -1,37 +1,41 @@
 package aufgabenblaetter.blatt1;
 
-
+/*
+ * tt, 14.11.: Eine separate Klasse für das Ausschreiben auf die Kommandozeile ist prinzipiell eine gute Idee. 
+ * Java bietet da auch schon einige verschiedene Varianten (sogenannte Logger), kannst du ja mal googlen. 
+ * Z.B. gibt es welche, die dir vor jedem Kommentar anzeigen, aus welcher Klasse und welcher Zeile es stammt (hilfreich für komplexe Programme). 
+ * In deinem Fall, wäre es einfacher gewesen, alle Console-Aufrufe durch System.out.println(…) zu ersetzen.
+ * Falls du nur keine Lust auf das Getippe hattest: Das geht auch gut per Autovervollständigung mit syso. ;)
+ */
 public class Console {
-    public static void log(float value) {
-        String output;
-        output = String.valueOf(value);
-        System.out.println(output);
-    }
+	public static void log(float value) {
+		String output;
+		output = String.valueOf(value);
+		System.out.println(output);
+	}
 
-    public static void log(int value) {
-        String output;
-        output = String.valueOf(value);
-        System.out.println(output);
-    }
+	public static void log(int value) {
+		String output;
+		output = String.valueOf(value);
+		System.out.println(output);
+	}
 
-    public static void log(char value) {
-        String output;
-        output = String.valueOf(value);
-        System.out.println(output);
-    }
+	public static void log(char value) {
+		String output;
+		output = String.valueOf(value);
+		System.out.println(output);
+	}
 
-    public static void log(String value) {
-        String output;
-        output = String.valueOf(value);
-        System.out.println(output);
-    }
+	public static void log(String value) {
+		String output;
+		output = String.valueOf(value);
+		System.out.println(output);
+	}
 
-    public static void log(Integer value) {
-        String output;
-        output = String.valueOf(value);
-        System.out.println(output);
+	public static void log(Integer value) {
+		String output;
+		output = String.valueOf(value);
+		System.out.println(output);
 
-    }
+	}
 }
-
-
--- src/aufgabenblaetter/blatt1/SquareRoot.java
+++ src/aufgabenblaetter/blatt1/SquareRoot.java
@@ -3,16 +3,26 @@ package aufgabenblaetter.blatt1;
 import java.lang.String;
 import java.util.Scanner;
 
+/* 
+ * Feedback, tt, 14.11.: 
+ * Sehr schön! Sauberer Stil und funktionaler Code. Hier und da kleine Anmerkungen, wie es noch besser wird. :)
+ */
 public class SquareRoot {
 
     public static void main(String[] args) {
         float value = userInput();
+        /* tt, 14.11.: Variablen mit '_' anfangen zu lassen ist unüblich. 
+         * Unterstriche verwendet man eigentlich nur bei Konstanten, um Wörter zu trennen (z.B. DIES_IST_EINE_KONSTANTE)
+         */
         float _result = squareRootCalculation(value);
         Console.log("This is the result as a float " + _result);
         Integer result = Math.round(_result); // parse to nearest integer
         Console.log("Result as an integer: " + result);
     }
 
+    /* tt, 14.11.: schön, dass du das aus der Übung aufgegriffen hast! 
+     * Hast du mal probiert was du ändern musst, damit man es nicht immer neu starten muss, sondern gleich die nächste Abfrage kommt?
+     */
     static float userInput() {
         System.out.println("Please provide a positive real number to determine its square root.");
         String input;
@@ -29,6 +39,7 @@ public class SquareRoot {
                 }
             } catch (Exception err) {
                 err.printStackTrace();
+                // tt, 14.11.: du erlaubst doch float, nicht nur integer ;)
                 Console.log("Provided value is invalid. Please provide a positive real number of type integer.");
             }
         }
@@ -36,12 +47,17 @@ public class SquareRoot {
     }
 
     private static float squareRootCalculation(float value) {
+    		// tt, 14.11.: funktioniert, aber dauert länger. eigentlich solltet ihr mit a/2, also hier value/2 starten.
         float prev = value; // with primitive
         float cur = -1;
         do {
+        		/* tt, 14.11.: schön, du hast den Fragezeichenoperator verstanden! :) 
+        		 * wenn du cur oben auch mit dem Anfangswert initialisierst, kannst du dir die Abfrage hier aber ganz sparen und stattdessen: prev=cur setzen.
+        		 * Probier es mal.
+        		 */
             prev = (cur != -1) ? cur : prev;
             cur = (prev + (value / prev)) / 2;
-        } while ((prev - cur) > 0.001);
+        } while ((prev - cur) > 0.001); // tt, 14.11.: eine Null vergessen... 0.0001
         return cur;
     }
 }

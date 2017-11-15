61015a9065aff32f98804ccb6a09c6f9ea32246e
Oksana Voitiuk <oksana@voituk.com>
2017-11-15 22:44:18 +0100

    Implement Scanner for reading from command line

--- src/uebung/ue3/Quadratwurzel.java
+++ src/uebung/ue3/Quadratwurzel.java
@@ -1,22 +1,42 @@
 package uebung.ue3;
 
+import java.util.Scanner;
+
 public class Quadratwurzel {
-
-	static double a = 36;
-
 	public static void main(String[] args) {
 
-		double startX = a / 2;
-		double buffer;
+		calculate();
+	}
 
-		do {
-			
-			buffer = startX;
-			startX = nextX(a, startX);
-		
-		} while ((buffer - startX) > 0.0001);
+	private static void calculate() {
 
-		System.out.println("The root of  " + a + " is " + startX);
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			int exitCode = 0;
+			System.out.println("Please give a positive number (or 0 for exit):");
+			int inputNumber = scanner.nextInt();
+			if (inputNumber < 0) {
+				System.out.println("Error.");
+				continue;
+			} else if (inputNumber == exitCode) {
+				break;
+			}
+
+			double value = Double.valueOf(inputNumber);
+			double startX = value / 2;
+			double buffer;
+
+			do {
+
+				buffer = startX;
+				startX = nextX(value, startX);
+
+			} while ((buffer - startX) > 0.0001);
+
+			System.out.println("The squeare root of " + value + " is " + startX);
+		}
+		scanner.close();
 	}
 
 	private static double nextX(double a, double x0) {

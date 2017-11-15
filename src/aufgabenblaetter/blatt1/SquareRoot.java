9bbf3bcc4035bd497aa8b539ec4d402aed7a084d
Andi.Go <Andi.Go@wlan-141-23-164-74.tubit.tu-berlin.de>
2017-11-15 10:28:37 +0100

    HA1

--- src/aufgabenblaetter/blatt1/SquareRoot.java
+++ src/aufgabenblaetter/blatt1/SquareRoot.java
@@ -9,14 +9,15 @@ public class SquareRoot {
 	public static void main(String[] args) {
 		// TODO bearbeite hier die Aufgabe aus dem freiwilligen Aufgabenblatt 1 und commite deine Loesung in deinem Branch. Viel Spaß :)
 		Scanner scanner = new Scanner(System.in); 
-		double a = scan(); 
+		double wert; // Wie sag ich ihm dass es der Wert sein soll den der user eingibt? 
 		System.out.println("Gib eine Zahl größer 0 an, deren Quadratwurzel ermittlet werden soll: "); 
-		
+		double ergebnis = Rechner(wert); 
+		System.out.println("Ihr Ergebnis: "+ ergebnis);
 	}
 	
 	//Rechner Klasse, die die eigentliche Schätzung durchführt. 
-	public static double Rechner(double a) {
-		double jetzt = a; 
+	public static double Rechner(double wert) {
+		double jetzt = wert; 
 		double vorher = -1; 
 		if ((vorher - jetzt) > 0.0001) {
 			jetzt = (vorher + jetzt/2) / 2; 

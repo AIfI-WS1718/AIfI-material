d0df0b70b6ba04e60fe78ee1d10407e4a5fc8c13
llassen <33228449+llassen@users.noreply.github.com>
2017-11-15 17:24:27 +0100

    Update SquareRoot.java

--- src/aufgabenblaetter/blatt1/SquareRoot.java
+++ src/aufgabenblaetter/blatt1/SquareRoot.java
@@ -3,8 +3,18 @@ package aufgabenblaetter.blatt1;
 public class SquareRoot {
 
 	public static void main(String[] args) {
-		// TODO bearbeite hier die Aufgabe aus dem freiwilligen Aufgabenblatt 1 und commite deine Loesung in deinem Branch. Viel Spaß :)
+		final double a = 25;
+		double b = a/2;
+		double f = 0.0001;
+		double e = 0;
+		double d = a;
 		
+		while(d > f) {
+			e = (b + (a/b)) / 2;
+			System.out.println("Das Ergebnis lautet: " + e);
+			d = Math.abs(e - b);
+			b = e;
+		}
 	}
 
 }

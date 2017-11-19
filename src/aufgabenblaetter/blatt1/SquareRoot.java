5639380cd28bbc972860148fd3ba8fd212574eaf
mathews <mathews@der-kleine-15.fritz.box>
2017-11-19 14:14:08 +0100

    1.Aufgabenblatt

--- src/aufgabenblaetter/blatt1/SquareRoot.java
+++ src/aufgabenblaetter/blatt1/SquareRoot.java
@@ -2,8 +2,21 @@ package aufgabenblaetter.blatt1;
 
 public class SquareRoot {
 
+
 	public static void main(String[] args) {
-		// TODO bearbeite hier die Aufgabe aus dem freiwilligen Aufgabenblatt 1 und commite deine Loesung in deinem Branch. Viel Spaß :)
+		double a=9.0;
+		double x=6.0;
+		double y=0.0;
+		double z=6.0;
+		
+		while (x-y >= 0.0001)
+		{
+		x=z;
+		y=(x+(a/x))/2;
+		z=y;
+		}
+		
+		System.out.println(x);
 		
 	}
 

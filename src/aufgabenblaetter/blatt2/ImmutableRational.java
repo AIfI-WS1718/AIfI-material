f1b033fffc159892a194146c6f56927bca154a56
Tim <tim.mathews@web.de>
2017-11-20 19:27:03 +0100

    2.Aufgabenblatt

--- src/aufgabenblaetter/blatt2/ImmutableRational.java
+++ src/aufgabenblaetter/blatt2/ImmutableRational.java
@@ -2,6 +2,75 @@ package aufgabenblaetter.blatt2;
 
 class ImmutableRational {
 
-	// TODO ... und hier hast du Platz fuer Aufgabe 1.2
+	public static void main(String[] args) {
+		ImmutableRational r = new ImmutableRational();
+		r.printRational();
+		r.toDouble();
+		r.add(10, 20);
+		ImmutableRational rl = new ImmutableRational(3,6);
+		rl.printRational();
+		r.reduce();
+		r.invert();
+		r.printRational();
+		r.negate();
+		r.printRational();
+		
+	}
+
+	Integer zaehler;
+	Integer nenner;
+	
+
+	public ImmutableRational () {
+		this.zaehler=10;
+		this.nenner=20;
+	}
+	public ImmutableRational (Integer z, Integer n) {
+		this.zaehler=z;
+		this.nenner=n;
+	}
+	public void printRational() {
+	System.out.println(zaehler + "/" + nenner);
+	}
+	public void negate() {
+		this.nenner = -this.nenner;
+		this.zaehler = -this.zaehler;
+	}
+	public void invert() {
+		Integer u;
+		u = this.nenner;
+		this.nenner = this.zaehler;
+		this.zaehler = u;
+	}
+	public void toDouble() {
+		double da;
+		double db;
+		double wert;
+		da = this.zaehler;
+		db = this.nenner;
+		wert = da/db;
+		System.out.println("Doublewert: " + wert);
+	}
+	public void reduce () {
+		if (this.zaehler<this.nenner && this.nenner%this.zaehler == 0) {
+			this.nenner=this.nenner/this.zaehler;
+			this.zaehler=this.zaehler/this.zaehler;
+		}
+		else if (this.zaehler>this.nenner && this.zaehler%this.nenner == 0) {
+			this.zaehler=this.zaehler/this.nenner;
+			this.nenner=this.nenner/this.nenner;
+		}
+		else if (this.zaehler%2 == 0 && this.nenner%2 ==0) {
+			this.zaehler=this.zaehler/2;
+			this.nenner=this.nenner/2;
+		}
+		
+		System.out.println("Gekürtzt: " + this.zaehler + "/" + this.nenner);
+	}
+	public void add(Integer v, Integer w) {
+		this.zaehler += v;
+		this.nenner += w;
+		System.out.println("Add: " + this.zaehler + "/" + this.nenner);
+	}
 	
 }
--- src/aufgabenblaetter/blatt2/Rational.java
+++ src/aufgabenblaetter/blatt2/Rational.java
@@ -1,7 +1,86 @@
 package aufgabenblaetter.blatt2;
 
 class Rational {
-
-	// TODO bearbeite hier die Aufgabe 1.1 aus dem freiwilligen Aufgabenblatt 2 und commite+pushe deine Loesung in deinem Branch. Viel Spaß!
 	
+	public static void main(String[] args) {
+		Rational r = new Rational();
+		r.setzaehler(10);
+		r.setnenner(20);
+		r.printRational();
+		r.toDouble();
+		r.add(10, 20);
+		Rational rl = new Rational(3,6);
+		rl.printRational();
+		r.reduce();
+		r.invert();
+		r.printRational();
+		r.negate();
+		r.printRational();
+		
+	}
+
+	int zaehler;
+	int nenner;
+	
+
+	public Rational () {
+		this.zaehler=0;
+		this.nenner=1;
+	}
+	public Rational (int z, int n) {
+		this.zaehler=z;
+		this.nenner=n;
+	}
+	public void printRational() {
+	System.out.println(zaehler + "/" + nenner);
+	}
+	public void setnenner(int nenner) {
+		this.nenner=nenner;
+	}
+	public void setzaehler(int zaehler) {
+		this.zaehler=zaehler;
+	}
+	public void negate() {
+		this.nenner = -this.nenner;
+		this.zaehler = -this.zaehler;
+	}
+	public void invert() {
+		int u;
+		u = this.nenner;
+		this.nenner = this.zaehler;
+		this.zaehler = u;
+	}
+	public void toDouble() {
+		double da;
+		double db;
+		double wert;
+		da = this.zaehler;
+		db = this.nenner;
+		wert = da/db;
+		System.out.println("Doublewert: " + wert);
+	}
+	public void reduce () {
+		int a,b;
+		a=this.zaehler;
+		b=this.nenner;
+		if (a<b && b%a == 0) {
+			b=b/a;
+			a=a/a;
+		}
+		else if (a>b && a%b == 0) {
+			a=a/b;
+			b=b/b;
+		}
+		else if (a%2 == 0 && b%2 ==0) {
+			a=a/2;
+			b=b/2;
+		}
+		
+		System.out.println("Gekürtzt: " + a + "/" + b);
+	}
+	public void add(int v, int w) {
+		this.zaehler += v;
+		this.nenner += w;
+		System.out.println("Add: " + this.zaehler + "/" + this.nenner);
+	}
 }

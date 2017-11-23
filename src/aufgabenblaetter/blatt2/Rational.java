a8036118b999e535373efc2a1feea5e4e0b6f559
doktorfaustus <doktorfaustus@github.com>
2017-11-23 21:06:33 +0100

    ha3

--- src/aufgabenblaetter/blatt2/Rational.java
+++ src/aufgabenblaetter/blatt2/Rational.java
@@ -1,7 +1,57 @@
 package aufgabenblaetter.blatt2;
 
 class Rational {
+	private double zaehler;
+	private double nenner;
 
-	// TODO bearbeite hier die Aufgabe 1.1 aus dem freiwilligen Aufgabenblatt 2 und commite+pushe deine Loesung in deinem Branch. Viel Spaß!
+	public Rational(){
+		zaehler = 0;
+		nenner = 1;
+	}
+	public Rational(double zaehler, double nenner) {
+		this.setNenner(nenner);
+		this.setZaehler(zaehler);
+	}
+	
+	public void printRational() {
+		System.out.println(zaehler + " / " + nenner);
+	}
+	
+	public void setZaehler(double zaehler) {
+		this.zaehler = zaehler;
+	}
+	public void setNenner(double nenner) {
+		this.nenner = nenner;
+	}
+	
+	public static void main(String[] args) {
+		Rational foo = new Rational();
+		foo.setZaehler(4);
+		foo.setNenner(5);
+		foo.printRational();
+		foo.negate();
+		foo.printRational();
+	}
+	public void invert() {
+		double temp = this.zaehler;
+		this.setZaehler(this.nenner);
+		this.setNenner(temp);
+	}
+	public void negate() {
+		this.setZaehler(-this.zaehler);
+	}
+	public double toDouble() {
+		return(zaehler/nenner);
+	}
+	public Rational reduce() {
+		Rational reducedRational = new Rational();
+		return(reducedRational);
+		//TODO
+	}
+	public add(Rational bob) {
+		Rational resultRational = new Rational();
+		resultRational.setZaehler
+		
+	}
 	
 }

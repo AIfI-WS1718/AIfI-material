9b4835141d7accc9695e61190238cf93cb643858
Gregor <Gregor@DESKTOP-SLBBFKB>
2017-11-28 12:34:31 +0100

    Korrektur HA2

--- src/aufgabenblaetter/blatt2/Rational.java
+++ src/aufgabenblaetter/blatt2/Rational.java
@@ -1,6 +1,11 @@
 package aufgabenblaetter.blatt2;
 
+/*
+ * Feedback gl 28.11.
+ * Kurz und gut. Sauberer Stil und funktional. Leider nicht alle Aufgabenteile bearbeitet.
+ */
 class Rational {
+	// gl 28.11. Üblicherweise sind Zähler und Nenner ganzzahlig. Also wären ganzzahlige Datentypen hier schöner.
 	private double zaehler;
 	private double nenner;
 
@@ -37,8 +42,9 @@ class Rational {
 		this.setZaehler(this.nenner);
 		this.setNenner(temp);
 	}
+	// gl 28.11. Bravo :-) Viele andere haben das Vorzeichen des Zählers und das des Nenners gedreht. Du hast es nicht nicht richtig ;-)
 	public void negate() {
-		this.setZaehler(-this.zaehler);
+		this.setZaehler(-this.zaehler); // gl 28.11. Es geht sogar noch kürzer : this.zaehler *= -1;
 	}
 	public double toDouble() {
 		return(zaehler/nenner);
@@ -54,8 +60,13 @@ class Rational {
 		resultRational.zaehler = this.zaehler * bob.nenner;
 		resultRational.nenner = this.nenner * bob.nenner;
 		resultRational.zaehler += bob.zaehler * this.nenner;
+		/* gl 28.11. Schön, dass du an das Konzept der Setter gedacht hast. Allerdings verwendet 
+		 * man die meist nicht innerhalb der Klasse in der man sie definiert hat, denn dort 
+		 * sind sowieso alle in dieser Klasse definierten Variablen sichtbar. Anstattdessen legt
+		 * man Setter an, um die Zugriffsmöglichkeiten aus anderen Klassen heraus zu regeln.
+		 */
 		this.setNenner(resultRational.nenner);
-		this.setZaehler(resultRational.zaehler); //wtf
+		this.setZaehler(resultRational.zaehler); //wtf    gl 28.11. genau, einfach hier direkt mit der Variable ohne Setter arbeiten
 	}
 	
 }

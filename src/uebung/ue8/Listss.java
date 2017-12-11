f4ec9329c95558fbbaa443b4d39fcf21c44958e0
Michael Koch <michael.koch@campus.tu-berlin.de>
2017-12-11 19:36:59 +0100

    Uebung char

--- src/uebung/ue8/Listss.java
+++ src/uebung/ue8/Listss.java
@@ -11,17 +11,22 @@ public class Listss {
 		List<String> arraylist = new ArrayList<>();
 		List<String> linkedlist = new LinkedList<>();
 
-		long n = 465000;
+		long n = 10000000;
+		double t1 = System.currentTimeMillis();
 		for (long l = 0; l < 10 * n; l++) {
 			arraylist.add("aString" + l);
 			linkedlist.add("lString" + l);
 			System.out.println("Aktuelle Position: " + l);
 		}
 
-		double t1 = System.currentTimeMillis();
+		double t2 = System.currentTimeMillis();
+		
+		System.out.println("Das Anlegen dauerte " + (int) (t2-t1) + " ms.");
+		
+		t1 = System.currentTimeMillis();
 		System.out.println(arraylist.get((int) (5 * n)));
 
-		double t2 = System.currentTimeMillis();
+		t2 = System.currentTimeMillis();
 		System.out.println("ArrayList: Die Abfrage dauerte " + (int) (t2 - t1) + " ms.");
 
 		t1 = System.currentTimeMillis();
@@ -33,32 +38,32 @@ public class Listss {
 		t1 = System.currentTimeMillis();
 		arraylist.add((int) (2 * n), "AAnfang");
 		t2 = System.currentTimeMillis();
-		System.out.println("ArrayList: Das Hinzufügen dauerte (Anfang) " + (int) (t2 - t1) + " ms.");
+		System.out.println("ArrayList: Das Hinzuf"+ (char) 252 + "gen dauerte (Anfang) " + (int) (t2 - t1) + " ms.");
 
 		t1 = System.currentTimeMillis();
 		arraylist.add((int) (5 * n), "AMitte");
 		t2 = System.currentTimeMillis();
-		System.out.println("ArrayList: Das Hinzufügen dauerte (Mitte) " + (int) (t2 - t1) + " ms.");
+		System.out.println("ArrayList: Das Hinzuf"+ (char) 252 + "gen dauerte (Mitte) " + (int) (t2 - t1) + " ms.");
 
 		t1 = System.currentTimeMillis();
 		arraylist.add((int) (10 * n), "AEnde");
 		t2 = System.currentTimeMillis();
-		System.out.println("ArrayList: Das Hinzufügen dauerte (Ende) " + (int) (t2 - t1) + " ms.");
+		System.out.println("ArrayList: Das Hinzuf"+ (char) 252 + "gen dauerte (Ende) " + (int) (t2 - t1) + " ms.");
 
 		t1 = System.currentTimeMillis();
 		linkedlist.add((int) (2 * n), "LAnfang");
 		t2 = System.currentTimeMillis();
-		System.out.println("LinkedList: Das Hinzufügen dauerte (Anfang) " + (int) (t2 - t1) + " ms.");
+		System.out.println("LinkedList: Das Hinzuf"+ (char) 252 + "gen dauerte (Anfang) " + (int) (t2 - t1) + " ms.");
 
 		t1 = System.currentTimeMillis();
 		linkedlist.add((int) (5 * n), "LMitte");
 		t2 = System.currentTimeMillis();
-		System.out.println("LinkedList: Das Hinzufügen dauerte (Mitte) " + (int) (t2 - t1) + " ms.");
+		System.out.println("LinkedList: Das Hinzuf"+ (char) 252 + "gen dauerte (Mitte) " + (int) (t2 - t1) + " ms.");
 
 		t1 = System.currentTimeMillis();
 		linkedlist.add((int) (10 * n), "LEnde");
 		t2 = System.currentTimeMillis();
-		System.out.println("LinkedList: Das Hinzufügen dauerte (Ende) " + (int) (t2 - t1) + " ms.");
+		System.out.println("LinkedList: Das Hinzuf"+ (char) 252 + "gen dauerte (Ende) " + (int) (t2 - t1) + " ms.");
 
 	}
 

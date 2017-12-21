46f5ce1ee62e9aaf1359e396c650722d3d026f96
Michael Koch <michael.koch@campus.tu-berlin.de>
2017-12-21 16:59:03 +0100

    Destroyable implementiert, Funktionen gefuellt mit Inhalt

--- src/aufgabenblaetter/blatt4/SpaceShip.java
+++ src/aufgabenblaetter/blatt4/SpaceShip.java
@@ -1,26 +1,50 @@
 package aufgabenblaetter.blatt4;
 
-public final class SpaceShip extends GalaxyElement implements Movable {
+public final class SpaceShip extends GalaxyElement implements Movable, Destroyable {
 
 	int speed;
-	
+	double energy = 100.0;
+
 	public SpaceShip(int x, int y, String name, double groesse, int speed) {
-		super(x,y,name,groesse);
+		super(x, y, name, groesse);
 		this.speed = speed;
 	}
-	
+
 	public SpaceShip(String name) {
 		super((int) Math.random() * 100, (int) Math.random() * 100, name, Math.random() * 100);
 		this.speed = 5;
 	}
-	
+
 	@Override
 	public void move() {
-		
+
 		this.x += speed;
 		this.y += speed;
 		System.out.println("move space ship");
-		
+
+	}
+
+	@Override
+	public void hit(double power) {
+		energy -= power;
+		System.out.println("Das SpaceShip hat noch eine Energie von: " + energy);
+		String a;
+		if (isDestroyed()) {
+			a = "";
+		}
+		else {
+			a = "nicht ";
+		}
+		System.out.println("Das Raumschiff ist " + a + "zerst" + (char) 246 + "rt.");
+	}
+
+	@Override
+	public boolean isDestroyed() {
+		if (energy <= 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }

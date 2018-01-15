65aefd2a62901fd1bb749243b81f7b9187401187
jakob <jakob@wlan-141-23-177-41.tubit.tu-berlin.de>
2018-01-15 16:56:00 +0100

    test

--- src/uebung/ue10/MyWurzel.java
+++ src/uebung/ue10/MyWurzel.java
@@ -21,7 +21,7 @@ public class MyWurzel {
 		if (result == n) {
 			return "Die Wurzel aus " + n + " ist " + sqr;
 		} else {
-			return "Keine Wurzel f�r " + n + " gefunden. Die n�chst gr��ere Zahl mit einer "
+			return "Keine Wurzel f�r " + n + " gefunden. Die n�chst egr��ere Zahl mit einer "
 					+ "Wurzel in den nat�rlichen Zahlen ist " + result + ". Die Wurzel ist " + sqr;
 		}
 	}

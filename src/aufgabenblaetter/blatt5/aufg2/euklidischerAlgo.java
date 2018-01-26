f8aeb3c0e8116eb048fb82b991d310d10e9d1607
tthunig <thunig@vsp.tu-berlin.de>
2018-01-26 13:32:18 +0100

    dateien fuer aufgabenblatt 5

---
+++ src/aufgabenblaetter/blatt5/aufg2/euklidischerAlgo.java
@@ -0,0 +1,11 @@
+package aufgabenblaetter.blatt5.aufg2;
+
+public class euklidischerAlgo {
+
+	public static void main(String[] args) {
+		// TODO bitte fuellen
+	}
+	
+	// TODO ggT(...) implementieren
+	
+}
